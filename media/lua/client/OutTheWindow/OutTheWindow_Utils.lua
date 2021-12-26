bbbf49dfa5598d25ec64d489c4d572dd06ea670d
Corentin Delorme <codelorme@gmail.com>
2021-12-26 09:58:05 +0100

    Refactor OutTheWindow_Utils.lua

--- media/lua/client/OutTheWindow/OutTheWindow_Utils.lua
+++ media/lua/client/OutTheWindow/OutTheWindow_Utils.lua
@@ -1,41 +1,34 @@
-local function getNorth(window)
-	return window.getNorth and window:getNorth() or window:isNorthHoppable()
-end
-
-local function getOppositeSquare(window, x, y, z)
-	return getNorth(window) and getSquare(x, y - 1, z) or getSquare(x - 1, y, z)
-end
-
-local function getDropSquare(player, window)
-
-	local dropSquare, floorSquare, oppositeSquare
-
-	local playerSquare = player:getSquare()
-	local windowSquare = window:getSquare()
-	local x, y, z = windowSquare:getX(), windowSquare:getY(), windowSquare:getZ()
-	local oppositeSquare = getOppositeSquare(window, x, y, z)
-
-	if getNorth(window) then
-		dropSquare = getSquare(x, y + 1, z)
-	else
-		dropSquare = getSquare(x + 1, y, z)
-	end
-
-	local dropSquare = playerSquare:equals(windowSquare) and oppositeSquare or dropSquare
-
-	x = dropSquare:getX()
-	y = dropSquare:getY()
-
-	for z = dropSquare:getZ(), 0, -1 do
-		floorSquare = getSquare(x, y, z)
-		if floorSquare:isSolidFloor() then
-			return floorSquare
-		end
-	end
-
-	error('This should never happen!')
-end
-
 return {
-	getDropSquare = getDropSquare,
+	getDropSquare = function(player, window)
+
+		local sq1, sq2, selected
+
+		local playerSquare = player:getSquare()
+		local px, py, pz = playerSquare:getX(), playerSquare:getY(), playerSquare:getZ()
+
+		local windowSquare = window:getSquare()
+		local wx, wy, wz = windowSquare:getX(), windowSquare:getY(), windowSquare:getZ()
+
+		if window.getNorth and window:getNorth() or window:isNorthHoppable() then
+			sq1 = getSquare(wx, wy + 1, wz)
+			sq2 = getSquare(wx, wy - 1, wz)
+			selected = (py >= wy) and sq2 or sq1
+		else
+			sq1 = getSquare(wx + 1, wy, wz)
+			sq2 = getSquare(wx - 1, wy, wz)
+			selected = (px >= wx) and sq2 or sq1
+		end
+
+		x = selected:getX()
+		y = selected:getY()
+
+		for z = selected:getZ(), 0, -1 do
+			local floorSquare = getSquare(x, y, z)
+			if floorSquare:isSolidFloor() then
+				return floorSquare
+			end
+		end
+
+		error('This should never happen!')
+	end
 }
